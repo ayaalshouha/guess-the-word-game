467e705bd42296cb49f17c1735f74eddd86ad568
Aya Al-Shouha <ayaalshouha12@gmail.com>
2024-07-30 09:24:24 +0300

    setting game_name

---
+++ main.js
@@ -0,0 +1,7 @@
+// setting game name
+let gama_name = "Guess The Word";
+document.title = gama_name;
+document.querySelector("h1").innerHTML = game_name;
+document.querySelector(
+  "footer"
+).innerHTML = `${game_name} Created By Aya AlShouha`;
