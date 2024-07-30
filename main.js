288bd916696e29f35eba67f7fbb49fe0dad4842f
Aya Al-Shouha <ayaalshouha12@gmail.com>
2024-07-30 09:27:43 +0300

    edit footer

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // setting game name
-let gama_name = "Guess The Word";
-document.title = gama_name;
+let game_name = "Guess The Word";
+document.title = game_name;
 document.querySelector("h1").innerHTML = game_name;
 document.querySelector(
   "footer"
-).innerHTML = `${game_name} Created By Aya AlShouha`;
+).innerHTML = `${game_name} Game created by Aya AlShouha`;
