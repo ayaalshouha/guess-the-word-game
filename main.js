55444bb224e6eea1ab99c4d37f8f91595fed78a9
Aya Al-Shouha <ayaalshouha12@gmail.com>
2024-07-30 15:56:51 +0300

    style inputs

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ let current_try = 1;
 function generateInputs() {
   const input_container = document.querySelector(".inputs");
 
+  // Create main try div with numbers for each trial
+
   for (let i = 1; i <= number_of_trials; i++) {
     const try_div = document.createElement("div");
     try_div.classList.add(`try-${i}`);
@@ -21,16 +23,19 @@ function generateInputs() {
 
     if (i !== 1) try_div.classList.add = "disabled";
 
+    //create inputs for each trial
     for (let j = 1; j <= number_of_letters; j++) {
       const input = document.createElement("input");
       input.type = "text";
       input.id = `try-${i}-letter-${j}`;
-      input.maxLength = 1; 
-      try_div.appendChild= input;
+      input.maxLength = 1;
+      try_div.appendChild(input);
     }
 
     input_container.appendChild(try_div);
   }
+  
+  input_container.children[0].children[1].focus();
 }
 
 window.onload = function () {
